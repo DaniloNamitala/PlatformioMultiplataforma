2c924a8ab6fd21b6dcb765b1db4066f2e0ccbfbb
Danilo Namitala <danilonamitala@yahoo.com.br>
2023-10-13 21:34:43 -0300

    Exemplos ifdef

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,32 @@
 #include <Arduino.h>
 
+// exemplo uso ifdef
 #ifdef BUILD_ARDUINO
 #define LED 13
 #elif BUILD_BLUEPILL
 #define LED PC13
 #endif
 
+// exemplo uso ifdef
+#ifdef BUILD_ARDUINO
+void config_duino() {
+  // essa funcao so vai existir no arduino
+}
+#elif BUILD_BLUEPILL
+void config_stm() {
+  // essa funcao so vai existir no stm
+}
+#endif  
+
 void setup() {
   pinMode(LED, OUTPUT);
+
+  // exemplo uso ifdef
+  #ifdef BUILD_ARDUINO
+  config_duino();
+  #elif BUILD_BLUEPILL
+  config_stm();
+  #endif
 }
 
 void loop() {
@@ -16,6 +35,7 @@ void loop() {
   digitalWrite(LED, LOW);
   delay(500);
 
+  // exemplo uso ifdef
   #ifdef BUILD_ARDUINO
   analogWrite(A0, 255);
   #elif BUILD_BLUEPILL
